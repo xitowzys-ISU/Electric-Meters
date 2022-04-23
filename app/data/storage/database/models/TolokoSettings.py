2259423cd1d65b6329322c3ceb6728097928b6ee
Kalinka George <xitowzys@gmail.com>
2022-04-24 02:39:50 +0800

    ⚙️ chore: toloko settings table changed

--- app/data/storage/database/models/TolokoSettings.py
+++ app/data/storage/database/models/TolokoSettings.py
@@ -10,10 +10,10 @@ class TolokoSettings(Base):
     __tablename__ = 'toloka_settings'
 
     id = sa.Column(INTEGER(unsigned=True),
-                   primary_key=True, autoincrement=True, nullable=False)
+                   primary_key=True, autoincrement=True)
     name = sa.Column(sa.VARCHAR(length=255), nullable=True)
-    project_id = sa.Column(INTEGER(unsigned=True), nullable=False)
+    project_id = sa.Column(INTEGER(unsigned=True), nullable=True)
     collection_pool_id = sa.Column(
-        INTEGER(unsigned=True), nullable=False)
+        INTEGER(unsigned=True), nullable=True)
     verification_pool_id = sa.Column(
-        INTEGER(unsigned=True), nullable=False)
+        INTEGER(unsigned=True), nullable=True)
